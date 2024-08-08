df1307c402c5d911e5089ff303b80bfc545460ab
mutefish0 <648262030@qq.com>
2024-08-09 00:59:28 +0800

    exclude

--- index.ts
+++ index.ts
@@ -1,6 +1,9 @@
 import { AssetsModule } from "jsr:@mutefish/deno-assets-module@0.0.2";
 import js from "./libs/web-audio.base64.ts";
-const rawBindings = await import(`./libs/${Deno.build.target}.base64.ts`);
+
+const rawBindings = await import(
+  `https://raw.githubusercontent.com/Mutefish0/web-audio-api/main/libs/${Deno.build.target}.base64.ts`
+);
 
 const am = new AssetsModule();
 await am.load(js);
