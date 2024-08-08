e18676367060677e70be0f546658961a4a5ee454
mutefish0 <648262030@qq.com>
2024-08-09 00:40:14 +0800

    update

---
+++ index.ts
@@ -0,0 +1,19 @@
+import { AssetsModule } from "jsr:@mutefish/deno-assets-module@0.0.2";
+import js from "./libs/web-audio.base64.ts";
+const rawBindings = await import(`./libs/${Deno.build.target}.base64.ts`);
+
+const am = new AssetsModule();
+await am.load(js);
+
+const processBindings = am.require("./node-web-audio.js");
+
+const tempFilePath = Deno.makeTempFileSync({
+  suffix: ".node",
+});
+Deno.writeFileSync(tempFilePath, rawBindings.default);
+const bindings = am.require(tempFilePath, true);
+Deno.removeSync(tempFilePath);
+
+const module = processBindings(bindings);
+
+export default module;
---
+++ mod.ts
@@ -0,0 +1,37 @@
+// Referencing the default lib web api typings
+/// <reference lib="dom" />
+import module from "./index.ts";
+
+export const AudioContext: typeof globalThis.AudioContext = module.AudioContext;
+export const OfflineAudioContext: typeof globalThis.OfflineAudioContext =
+  module.OfflineAudioContext;
+export const AudioBuffer: typeof globalThis.AudioBuffer = module.AudioBuffer;
+export const GainNode: typeof globalThis.GainNode = module.GainNode;
+export const PeriodicWave: typeof globalThis.PeriodicWave = module.PeriodicWave;
+export const MediaStreamAudioSourceNode: typeof globalThis.MediaStreamAudioSourceNode =
+  module.MediaStreamAudioSourceNode;
+export const AnalyserNode: typeof globalThis.AnalyserNode = module.AnalyserNode;
+export const AudioBufferSourceNode: typeof globalThis.AudioBufferSourceNode =
+  module.AudioBufferSourceNode;
+export const BiquadFilterNode: typeof globalThis.BiquadFilterNode =
+  module.BiquadFilterNode;
+export const ChannelMergerNode: typeof globalThis.ChannelMergerNode =
+  module.ChannelMergerNode;
+export const ChannelSplitterNode: typeof globalThis.ChannelSplitterNode =
+  module.ChannelSplitterNode;
+export const ConstantSourceNode: typeof globalThis.ConstantSourceNode =
+  module.ConstantSourceNode;
+export const ConvolverNode: typeof globalThis.ConvolverNode =
+  module.ConvolverNode;
+export const DelayNode: typeof globalThis.DelayNode = module.DelayNode;
+export const DynamicsCompressorNode: typeof globalThis.DynamicsCompressorNode =
+  module.DynamicsCompressorNode;
+export const IIRFilterNode: typeof globalThis.IIRFilterNode =
+  module.IIRFilterNode;
+export const OscillatorNode: typeof globalThis.OscillatorNode =
+  module.OscillatorNode;
+export const PannerNode: typeof globalThis.PannerNode = module.PannerNode;
+export const StereoPannerNode: typeof globalThis.StereoPannerNode =
+  module.StereoPannerNode;
+export const WaveShaperNode: typeof globalThis.WaveShaperNode =
+  module.WaveShaperNode;
