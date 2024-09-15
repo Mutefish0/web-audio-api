06492fc49461f6d6be9ff30ecde43349f1d5dac2
mutefish0 <648262030@qq.com>
2024-09-16 00:07:22 +0800

    zip all

--- index.ts
+++ index.ts
@@ -1,24 +1,11 @@
-import { AssetsModule } from "jsr:@mutefish/deno-assets-module@0.0.3";
-import js from "./web-audio.base64.ts";
-
-const rawBindings = await import(`./napi/${Deno.build.target}.base64.ts`);
+import { AssetsModule } from "jsr:@mutefish/deno-assets-module@0.0.4";
+import data from "./node-web-audio.base64.ts";
 
 const am = new AssetsModule();
-await am.load(js);
+await am.load(data);
 
-const processBindings = am.require("./node-web-audio.js");
-
-const tempFilePath = Deno.makeTempFileSync({
-  suffix: ".node",
-});
-Deno.writeFileSync(tempFilePath, rawBindings.default);
-const bindings = am.require(tempFilePath, true);
-
-try {
-  Deno.removeSync(tempFilePath);
-} catch (_) {
-  //
-}
+const processBindings = am.require("./js/node-web-audio.js");
+const bindings = am.require(`./libs/${Deno.build.target}.node`);
 
 const module = processBindings(bindings);
 
